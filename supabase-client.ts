03efbf1e70e7e136645b67411c16661acc7b01e0
Islamkandi1 <eslamkandil097@gmail.com>
2025-12-09 15:23:47 +0200

    types error

--- supabase-client.ts
+++ supabase-client.ts
@@ -1,4 +1,5 @@
-import { createClient } from "@supabase/supabase-js/src/index";
+
+import { createClient } from "@supabase/supabase-js";
 const URL = "https://cndbtgaifheduxginlre.supabase.co";
 const KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNuZGJ0Z2FpZmhlZHV4Z2lubHJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2NDQwNDIsImV4cCI6MjA3OTIyMDA0Mn0.UmycaPz6oqIYQLqezaUNl-sEhi4IyB_KPmpyO2rnvFY";
